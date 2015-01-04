ae58205353b345508ff05e7b37b64d64aa08a95e
gillchristian <gillchristiang@gmail.com>
2015-01-04 17:19:19 +0600

    MC

--- app/app.js
+++ app/app.js
@@ -70,33 +70,6 @@ angular.module('socialApp', ['ui.router', 'socialApp.controllers', 'socialApp.se
       });
 })
 
-// .config(function($routeProvider) {
-//    $routeProvider
-//       .when('/', {
-//          templateUrl: 'app/templates/home.html',
-//          controller: 'HomeCtrl'
-//       })
-//       .when('/todo', {
-//          templateUrl: 'app/templates/todo.html',
-//          controller: 'TodoController'
-//       })
-//       .when('/settings', {
-//          templateUrl: 'app/templates/settings.html',
-//          controller: 'SettingsController'
-//       })
-//       .when('/new', {
-//          templateUrl: 'app/templates/new.html',
-//          controller: 'NewController'
-//       })
-//       .when('/login', {
-//          templateUrl: 'app/templates/login.html',
-//          controller: 'LoginController'
-//       })
-//       .otherwise({
-//          redirectTo: '/'
-//       });
-// })
-
 .config(['$httpProvider', function($httpProvider) {
    $httpProvider.defaults.useXDomain = true;
    delete $httpProvider.defaults.headers.common['X-Requested-With'];
